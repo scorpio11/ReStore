8eefd22f876fa21c490fc74aac0bb33d1455f065
Eren <erenerciyes@gmail.com>
2023-03-12 22:52:31 +0300

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,10 +2,11 @@
 import { Container, CssBaseline, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "../models/product";
 import Header from "./Header";
-
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <>
+    <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
     <CssBaseline/>
       <Header/>
       <Container>
